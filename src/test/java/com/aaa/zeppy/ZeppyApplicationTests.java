e32d8d82b910f4d2e1fa590ecb1f44c167107b3b
Carrot_hyeong <rjgjfl90@naver.com>
2023-07-12 01:59:26 +0900

    init

---
+++ src/test/java/com/aaa/zeppy/ZeppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa.zeppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
