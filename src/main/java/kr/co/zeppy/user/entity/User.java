a04f0496e00203552ed655742c51149bca14bbea
Carrot_hyeong <rjgjfl90@naver.com>
2023-07-24 02:09:03 +0900

    Refactor: social login refactoring - 유저 패스워드 없애고 소셜로그인만 적용 시키는 중

--- src/main/java/kr/co/zeppy/user/entity/User.java
+++ src/main/java/kr/co/zeppy/user/entity/User.java
@@ -1,9 +1,22 @@
 package kr.co.zeppy.user.entity;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.util.LinkedHashSet;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,51 +31,34 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
-    private String email; // 이메일
-    private String password; // 비밀번호
-    private String nickname; // 닉네임
-    private String imageUrl; // 프로필 이미지
-    private int age;
-    private String city; // 사는 도시
+    private String email;
+    private String nickname;
+    private String imageUrl;
+    private LinkedHashSet<String> group;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Enumerated(EnumType.STRING)
-    private SocialType socialType; // KAKAO, NAVER, GOOGLE
+    private SocialType socialType; // KAKAO, GOOGLE
 
-    private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+    private String socialId;
 
-    private String refreshToken; // 리프레시 토큰
+    private String refreshToken;
 
-    // 유저 권한 설정 메소드
     public void authorizeUser() {
         this.role = Role.USER;
     }
 
-    // 비밀번호 암호화 메소드
-    public void passwordEncode(PasswordEncoder passwordEncoder) {
-        this.password = passwordEncoder.encode(this.password);
-    }
-
-    //== 유저 필드 업데이트 ==//
     public void updateNickname(String updateNickname) {
         this.nickname = updateNickname;
     }
 
-    public void updateAge(int updateAge) {
-        this.age = updateAge;
-    }
-
-    public void updateCity(String updateCity) {
-        this.city = updateCity;
-    }
-
-    public void updatePassword(String updatePassword, PasswordEncoder passwordEncoder) {
-        this.password = passwordEncoder.encode(updatePassword);
-    }
-
     public void updateRefreshToken(String updateRefreshToken) {
         this.refreshToken = updateRefreshToken;
+    }
+
+    public void updateImageUrl(String updateImageUrl) {
+        this.imageUrl = updateImageUrl;
     }
 }
