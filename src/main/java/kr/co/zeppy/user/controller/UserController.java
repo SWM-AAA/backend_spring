0f7b0fe16ee76ef351021b71690c3739840062bd
Carrot_hyeong <rjgjfl90@naver.com>
2023-07-28 11:31:31 +0900

    fix : /api 추가

--- src/main/java/kr/co/zeppy/user/controller/UserController.java
+++ src/main/java/kr/co/zeppy/user/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
