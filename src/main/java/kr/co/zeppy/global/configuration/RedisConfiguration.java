3d0cbc1141b6b609e7f28106f4fce73180d0bdf2
Carrot_hyeong <rjgjfl90@naver.com>
2023-07-28 14:01:19 +0900

    Fix : redis 관련 설정 추가 및 template 추가

--- src/main/java/kr/co/zeppy/global/configuration/RedisConfiguration.java
+++ src/main/java/kr/co/zeppy/global/configuration/RedisConfiguration.java
@@ -2,25 +2,19 @@ package kr.co.zeppy.global.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         final RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
 }
