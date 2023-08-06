ec550dee266812b042a866c8e33c77eac0ce07bb
Carrot_hyeong <rjgjfl90@naver.com>
2023-08-06 21:52:10 +0900

    [TAS-125] RestControllerAdvice로 spring MVC에 대한 전역 예외 처리 로직 생성

--- src/main/java/kr/co/zeppy/global/handler/GlobalExceptionHandler.java
+++ src/main/java/kr/co/zeppy/global/handler/GlobalExceptionHandler.java
@@ -1,21 +1,29 @@
 package kr.co.zeppy.global.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import kr.co.zeppy.global.dto.ErrorResponse;
-import kr.co.zeppy.global.error.ZeppyException;
+import kr.co.zeppy.global.error.ApplicationException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ZeppyException.class)
-    public ResponseEntity<ErrorResponse> handleZeppyException(ZeppyException e) {
-        log.error("handleZeppyException", e);
-        ErrorResponse errorResponse = ErrorResponse.of(e);
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ErrorResponse> handleApplicationException(ApplicationException e) {
+        log.error("handleApplicationException : ", e);
+        ErrorResponse errorResponse = ErrorResponse.fromException(e);
         return new ResponseEntity<>(errorResponse, e.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unexpected error : ", e);
+        ErrorResponse errorResponse = ErrorResponse.defaultError();
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
