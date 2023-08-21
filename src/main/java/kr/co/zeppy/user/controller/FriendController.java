2edec5a2c979063488dfafd0238f1f28634beeca
Carrot_hyeong <rjgjfl90@naver.com>
2023-08-21 21:11:27 +0900

    [TAS-138] 친구추가 컨트롤러 수정

--- src/main/java/kr/co/zeppy/user/controller/FriendController.java
+++ src/main/java/kr/co/zeppy/user/controller/FriendController.java
@@ -4,15 +4,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.zeppy.user.dto.FriendshipRequest;
+import kr.co.zeppy.user.service.FriendService;
+import lombok.RequiredArgsConstructor;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class FriendController {
+
+    private final FriendService friendService;
     
         // 친구 추가 요청 post
-    @PostMapping("/v1/users/send-friend-request")
+    @PostMapping("/v1/friends/requests")
     public ResponseEntity<Void> sendFriendRequest(@RequestHeader("Authorization") String token,
             @RequestBody FriendshipRequest friendshipRequest) {
+        
+        friendService.sendFriendRequest(token, friendshipRequest);
 
         return ResponseEntity.ok().build();
     }
