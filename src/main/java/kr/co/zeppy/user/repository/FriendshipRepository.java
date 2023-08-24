602ecd326b15d699dc65867879b9637fc4b2e6c7
Carrot_hyeong <rjgjfl90@naver.com>
2023-08-24 16:25:20 +0900

    [TAS-132] 친구 위치 뿌려주기 위한 jpa 생성

--- src/main/java/kr/co/zeppy/user/repository/FriendshipRepository.java
+++ src/main/java/kr/co/zeppy/user/repository/FriendshipRepository.java
@@ -1,9 +1,15 @@
 package kr.co.zeppy.user.repository;
+import java.util.List;
 import java.util.Optional;
 import kr.co.zeppy.user.entity.Friendship;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long>{
     Optional<Friendship> findByUserIdAndFriendId(Long userId, Long friendId);
+
+    @Query("SELECT f FROM Friendship f WHERE (f.user.id = :userId OR f.friend.id = :userId) AND f.status = 'ACCEPTED'")
+    List<Friendship> findAllFriendshipsByUserId(@Param("userId") Long userId);
 }
