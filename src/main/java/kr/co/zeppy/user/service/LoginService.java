24d2531556c3a00d7869b6aa87aa56a0b54579a9
Carrot_hyeong <rjgjfl90@naver.com>
2023-11-27 14:41:49 +0900

    feature : friend location service refactoring

--- src/main/java/kr/co/zeppy/user/service/LoginService.java
+++ src/main/java/kr/co/zeppy/user/service/LoginService.java
@@ -9,9 +9,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
 
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class LoginService implements UserDetailsService {
 
