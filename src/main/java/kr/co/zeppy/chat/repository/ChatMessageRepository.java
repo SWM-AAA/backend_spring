388321cddf5a03c498a84ac8481ce19ddad2f75a
Carrot_hyeong <rjgjfl90@naver.com>
2024-02-19 01:16:34 +0900

    [ZP-63] 임시 commit

---
+++ src/main/java/kr/co/zeppy/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,7 @@
+package kr.co.zeppy.chat.repository;
+
+import kr.co.zeppy.chat.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+}
